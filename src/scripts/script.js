a4c6180e7d7d7303b1afd6078498d5ee9272e4bf
Valentin <Chris_valentin@hotmail.com>
2023-05-23 14:14:09 +0200

    burgermenu 80% done

---
+++ src/scripts/script.js
@@ -0,0 +1,15 @@
+const burgerMenu = document.querySelector(".burgerMenu");
+const navItems = document.querySelector(".navItems");
+
+burgerMenu.addEventListener("mousedown", () => {
+  burgerMenu.classList.toggle("active");
+  navItems.classList.toggle("active");
+  console.log("Må du have en god dag!");
+});
+
+document.querySelectorAll(".navItem").forEach((n) =>
+  n.addEventListener("mousedown", () => {
+    burgerMenu.classList.remove("active");
+    navItems.classList.remove("active");
+  })
+);
