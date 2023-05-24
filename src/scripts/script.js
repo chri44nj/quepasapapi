7a70db30c744bfa1a684df9c46aac0b0f428494d
Valentin <Chris_valentin@hotmail.com>
2023-05-24 18:02:04 +0200

    burgermenu + slideshow 90% done

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,5 @@
+/* BURGER MENU */
+
 const burgerMenu = document.querySelector(".burgerMenu");
 const navItems = document.querySelector(".navItems");
 
@@ -13,3 +15,45 @@ document.querySelectorAll(".navItem").forEach((n) =>
     navItems.classList.remove("active");
   })
 );
+
+/* SLIDESHOW */
+
+var counter = 1;
+var totalSlides = 8; // Update the total number of slides here
+var intervalID;
+
+function startSlideshow() {
+  intervalID = setInterval(function () {
+    document.getElementById("radio" + counter).checked = true;
+    counter++;
+    if (counter > totalSlides) {
+      counter = 1;
+    }
+  }, 5000);
+}
+
+startSlideshow();
+
+var radioButtons = document.querySelectorAll('input[name="radioButton"]');
+var isManualNavigation = false;
+var clickedButtonIndex;
+
+radioButtons.forEach(function (radioButton, index) {
+  radioButton.addEventListener("click", function () {
+    clickedButtonIndex = index;
+    handleRadioButtonClick();
+  });
+});
+
+function handleRadioButtonClick() {
+  if (!isManualNavigation) {
+    isManualNavigation = true;
+    clearInterval(intervalID);
+    counter = clickedButtonIndex + 1; // Update counter to match the currently pressed radio button's index
+
+    setTimeout(function () {
+      isManualNavigation = false;
+      startSlideshow();
+    }, 10000); // 10 seconds
+  }
+}
